c8c986d0f31827424cb8bcd21805351a7733cf0a
Einzi1 <einzinger.simon@googlemail.com>
2019-04-04 12:23:42 +0200

    cors allowed

--- src/main/java/de/ergo/propertyinsurance/rest/InsuranceEnquiryRepository.java
+++ src/main/java/de/ergo/propertyinsurance/rest/InsuranceEnquiryRepository.java
@@ -3,7 +3,9 @@ package de.ergo.propertyinsurance.rest;
 import de.ergo.propertyinsurance.model.InsuranceEnquiry;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "insuranceEnquiries", path = "/insuranceEnquiries")
 public interface InsuranceEnquiryRepository extends MongoRepository<InsuranceEnquiry, String> {
 
